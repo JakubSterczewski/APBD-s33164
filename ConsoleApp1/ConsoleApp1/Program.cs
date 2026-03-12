a447ff5830f219511e9b0d060e11fbdb385b83df
Jakub Sterczewski <jakub.sterczewski6@gmail.com>
2026-03-12 11:05:04 +0100

    Add checking if entered value is a number

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,9 +2,26 @@
 {
     public static void Main(string[] args)
     {
-        Console.Write("Please enter a number: ");
-        string input = Console.ReadLine();
-        
-        Console.Write("Entered value: " + input);
+        int enteredNumber = GetNumberFromUser();
+        Console.Write("Entered value: " + enteredNumber);
+    }
+
+    public static int GetNumberFromUser()
+    {
+        while (true)
+        {
+            Console.Write("Please enter a number: ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int enteredNumber = int.Parse(input);
+                return enteredNumber;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
     }
 }
