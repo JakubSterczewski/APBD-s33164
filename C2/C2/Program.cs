225dc1653c68a3e6322a1780a56eaedf10d3a79c
Jakub Sterczewski <jakub.sterczewski6@gmail.com>
2026-03-25 17:09:54 +0100

    Add tests

--- C2/C2/Program.cs
+++ C2/C2/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using C2;
+
 Console.WriteLine("Hello, World!");
+
+RentalService rentalService = new RentalService();
+Student student = new Student("John", "Doe");
+Laptop laptop = new Laptop("Asus", "JakasNazwa", 12, "Procer nazwa");
+Laptop laptop2 = new Laptop("Asus", "JakasNazwa", 12, "Procer nazwa2");
+Laptop laptop3 = new Laptop("Asus", "JakasNazwa", 12, "Procer nazwa3");
+
+rentalService.RentEquipment(student, laptop);
+rentalService.RentEquipment(student, laptop2);
+rentalService.ReturnEquipment(laptop2);
+rentalService.RentEquipment(student, laptop2);
+rentalService.RentEquipment(student, laptop3);
